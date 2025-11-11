4edd8d61906ff1c3a3251341b6f92ca57dfe1ba9
Vikki2002 <kuswahavikki@gmail.com>
2025-11-11 10:23:29 +0530

    Add basic arithmetic operations and greeting functionality

---
+++ task1.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first number: "));
+b = int(input("Enter second number: "));
+
+# Additon
+addition = a + b;
+subtraction = a - b;
+multiplication = a * b;
+division = a / b;
+
+print("Addition:", addition);
+print("Subtraction:", subtraction);
+print("Multiplication:", multiplication);
+print("Division:", division);
